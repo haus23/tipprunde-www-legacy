90b6a38750670cd571160c6745d35b05506959ed
Micha Buchholz <micha@haus23.net>
2023-01-22 21:29:51 +0100

    Cleanup code and add branding.

--- apps/foh/app/root.tsx
+++ apps/foh/app/root.tsx
@@ -1,22 +1,15 @@
-import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import type { MetaFunction } from '@remix-run/node';
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from '@remix-run/react';
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+  charset: 'utf-8',
+  title: 'runde.tips',
+  viewport: 'width=device-width,initial-scale=1',
 });
 
 export default function App() {
   return (
-    <html lang="en">
+    <html lang="de">
       <head>
         <Meta />
         <Links />
