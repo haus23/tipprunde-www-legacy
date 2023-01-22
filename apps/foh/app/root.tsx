6a9265419ace39d23967d146ffca19fe1d0b001a
Micha Buchholz <micha@haus23.net>
2023-01-22 22:30:55 +0100

    Load championships in the root loader. No revalidation.

--- apps/foh/app/root.tsx
+++ apps/foh/app/root.tsx
@@ -7,6 +7,13 @@ export const meta: MetaFunction = () => ({
   viewport: 'width=device-width,initial-scale=1',
 });
 
+export async function loader() {
+  const response = await fetch(`${process.env.API_URL}/championships`);
+  return response.json();
+}
+
+export const shouldRevalidate = () => false;
+
 export default function App() {
   return (
     <html lang="de">
--- apps/foh/remix.env.d.ts
+++ apps/foh/remix.env.d.ts
@@ -1,2 +1,8 @@
 /// <reference types="@remix-run/dev" />
 /// <reference types="@remix-run/node" />
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    API_URL: string;
+  }
+}
