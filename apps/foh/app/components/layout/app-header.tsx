d1f72c47dba732742c4d3bac25b9d2cf025b37bd
Micha Buchholz <micha@haus23.net>
2023-01-23 23:51:47 +0100

    Styling app-header focus and hover states

--- apps/foh/app/components/layout/app-header.tsx
+++ apps/foh/app/components/layout/app-header.tsx
@@ -14,18 +14,31 @@ export function AppHeader() {
   return (
     <header className="flex items-center justify-between px-2 sm:px-4 bg-radix-mauve2 border-b border-radix-mauve6 shadow">
       <div className="flex items-center gap-x-8">
-        <Link to="/" className="flex items-center gap-x-2 py-4">
+        <Link to="/" className="group flex items-center gap-x-2 py-4 focus:outline-none">
           <Logo className="h-8 sm:h-10" />
-          <h1 className="text-xl font-semibold">runde.tips</h1>
+          <h1 className="p-1 sm:p-2 rounded-lg text-xl font-semibold group-focus:ring-4 group-focus:ring-radix-mauve7">
+            runde.tips
+          </h1>
         </Link>
         <nav className="hidden sm:flex self-stretch items-stretch gap-x-2">
           {navItems.map((item) => (
             <NavLink
               to={item.routeSegment}
               key={item.label}
-              className={({ isActive }) => cn('flex items-center')}
+              className={({ isActive }) =>
+                cn(
+                  isActive
+                    ? 'border-radix-violet8'
+                    : 'border-transparent hover:border-radix-mauve8',
+                  'group px-2 flex items-center focus:outline-none border-b-2 translate-y-[2px]'
+                )
+              }
             >
-              {({ isActive }) => <span>{item.label}</span>}
+              {({ isActive }) => (
+                <span className="p-1 sm:p-2 rounded-lg group-hover:bg-radix-mauve5 group-focus:ring-4 group-focus:ring-radix-mauve7">
+                  {item.label}
+                </span>
+              )}
             </NavLink>
           ))}
         </nav>
