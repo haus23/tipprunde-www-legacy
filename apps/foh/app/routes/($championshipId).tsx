47290a30a2b29172c075c7a38eb0eb34d9fc9d52
Micha Buchholz <micha@haus23.net>
2023-01-26 13:47:03 +0100

    Add main container

--- apps/foh/app/routes/($championshipId).tsx
+++ apps/foh/app/routes/($championshipId).tsx
@@ -17,7 +17,9 @@ export default function AppLayout() {
   return (
     <>
       <AppHeader />
-      <Outlet />
+      <main className="mx-auto max-w-5xl mt-6 sm:px-6 lg:px-8 pb-10">
+        <Outlet />
+      </main>
     </>
   );
 }
