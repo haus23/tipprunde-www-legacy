29e0488681d247bb91011e50f2802a2c9d3c2170
Micha Buchholz <micha@haus23.net>
2023-01-27 00:31:15 +0100

    Preformat the standings data

--- apps/foh/app/hooks/use-standings.ts
+++ apps/foh/app/hooks/use-standings.ts
@@ -1,6 +1,17 @@
 import { useRouteLoaderData } from '@remix-run/react';
+import { useMemo } from 'react';
 import type { Standings } from '~/queries/get-standings';
 
 export function useStandings() {
-  return useRouteLoaderData('routes/($championshipId)') as Standings;
+  const data = useRouteLoaderData('routes/($championshipId)') as Standings;
+
+  return useMemo(
+    () => ({
+      matches: [...data.matches].sort((a, b) => a.nr - b.nr),
+      players: [...data.players].sort((a, b) => a.rank - b.rank),
+      rounds: [...data.rounds].sort((a, b) => a.nr - b.nr),
+      tips: data.tips,
+    }),
+    [data]
+  );
 }
