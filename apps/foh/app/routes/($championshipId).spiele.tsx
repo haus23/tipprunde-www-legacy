f01faa0f36e23303c575537265a39e8fd43c2524
Micha Buchholz <micha@haus23.net>
2023-01-27 14:26:02 +0100

    Sorting tips.

--- apps/foh/app/routes/($championshipId).spiele.tsx
+++ apps/foh/app/routes/($championshipId).spiele.tsx
@@ -1,6 +1,14 @@
-import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
+import { ChampionshipTip } from '@haus23/dtp-types';
+import {
+  ArrowDownIcon,
+  ArrowUpIcon,
+  CheckIcon,
+  ChevronDownIcon,
+  ChevronUpDownIcon,
+  ChevronUpIcon,
+} from '@heroicons/react/24/outline';
 import * as Select from '@radix-ui/react-select';
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect, useReducer, useState } from 'react';
 
 import { useMasterdata } from '~/hooks/use-masterdata';
 import { useStandings } from '~/hooks/use-standings';
@@ -8,22 +16,73 @@ import { cn } from '~/utils/cn';
 
 export const handle = { view: 'spiele' };
 
+// Table row type
+type TipRow = Partial<ChampionshipTip> & {
+  name: string;
+  info: boolean;
+};
+
+// Feature Sorting
+type SortColumn = 'name' | 'tip' | 'points';
+type SortOrder = number; // 0 -> absteigend, 1 -> unsortiert, 1 -> aufsteigend
+type SortSpec = { column?: SortColumn; order: SortOrder };
+
+function sortReducer(state: SortSpec, column: SortColumn): SortSpec {
+  if (column !== state.column) {
+    return { column, order: 2 };
+  } else {
+    return { column, order: (state.order + 1) % 3 };
+  }
+}
+function sortTips(tips: TipRow[], { column, order }: SortSpec) {
+  // No sort column or order is 0 -> no sorting
+  if (!column || order === 1) return tips;
+
+  const missingTips = tips.filter((t) => !t.tip);
+  const sortableTips = tips.filter((t) => Boolean(t.tip));
+
+  switch (column) {
+    case 'name':
+      return [...tips].sort((a, b) => a.name.localeCompare(b.name) * (order - 1));
+    case 'points':
+      return [
+        ...sortableTips.sort((a, b) => ((b.points || 0) - (a.points || 0)) * (order - 1)),
+        ...missingTips,
+      ];
+    case 'tip':
+      return [
+        ...sortableTips.sort((a, b) => {
+          const aGoals = a.tip!.split(':').map(Number);
+          const aDiff = aGoals[0] - aGoals[1];
+          const bGoals = b.tip!.split(':').map(Number);
+          const bDiff = bGoals[0] - bGoals[1];
+
+          return (bDiff - aDiff || bGoals[0] - aGoals[0]) * (order - 1);
+        }),
+        ...missingTips,
+      ];
+  }
+}
+
 export default function Spiele() {
   const { championship, teams, players: allPlayers } = useMasterdata();
   const { rounds, matches, players, tips } = useStandings();
 
   const [selectedMatchId, setSelectedMatchId] = useState(matches[0].id);
 
-  const playerTips = players.map((p) => {
+  const playerTips: TipRow[] = players.map((p) => {
     const tip = tips.find((t) => t.matchId === selectedMatchId && t.playerId === p.id);
     return {
-      key: p.playerId,
+      ...tip,
       name: allPlayers[p.playerId].name,
-      tip: tip?.tip,
-      points: tip?.points,
-      info: tip?.joker || tip?.lonelyHit,
+      info: tip?.joker || tip?.lonelyHit || false,
     };
   });
+
+  const [sorting, changeSorting] = useReducer(sortReducer, {
+    column: undefined,
+    order: 0,
+  } as SortSpec);
 
   useEffect(() => {
     setSelectedMatchId(matches[0].id);
@@ -92,24 +151,51 @@ export default function Spiele() {
           </Select.Portal>
         </Select.Root>
       </div>
-      <table className="text-sm mt-8 w-full text-left">
-        <thead className="text-xs uppercase violet-bg">
+      <table className="text-sm mt-8 w-full">
+        <thead className="text-xs bg-radix-violet2">
           <tr>
-            <th scope="col" className="w-full font-medium text-left py-3 px-4 md:px-6">
-              Spieler
+            <th scope="col" className="w-full text-left py-3 px-4 md:px-6">
+              <button onClick={() => changeSorting('name')} className="relative">
+                <span className="font-medium uppercase pr-4">Spieler</span>
+                {sorting.column !== 'name' || sorting.order === 1 ? (
+                  <ChevronUpDownIcon className="absolute right-0 top-0 h-4 w-4" />
+                ) : sorting.order === 0 ? (
+                  <ArrowDownIcon className="absolute right-0 top-0 h-4 w-4" />
+                ) : (
+                  <ArrowUpIcon className="absolute right-0 top-0 h-4 w-4" />
+                )}
+              </button>
             </th>
-            <th scope="col" className="text-center font-medium px-4 md:px-6 ">
-              Tipp
+            <th scope="col" className="text-center px-4 md:px-6 ">
+              <button onClick={() => changeSorting('tip')} className="relative">
+                <span className="font-medium uppercase pr-4">Tipp</span>
+                {sorting.column !== 'tip' || sorting.order === 1 ? (
+                  <ChevronUpDownIcon className="absolute right-0 top-0 h-4 w-4" />
+                ) : sorting.order === 0 ? (
+                  <ArrowDownIcon className="absolute right-0 top-0 h-4 w-4" />
+                ) : (
+                  <ArrowUpIcon className="absolute right-0 top-0 h-4 w-4" />
+                )}
+              </button>
             </th>
-            <th scope="col" className="text-center font-medium px-4 md:px-6">
-              Punkte
+            <th scope="col" className="text-center px-4 md:px-6">
+              <button onClick={() => changeSorting('points')} className="relative">
+                <span className="font-medium uppercase pr-4">Punkte</span>
+                {sorting.column !== 'points' || sorting.order === 1 ? (
+                  <ChevronUpDownIcon className="absolute right-0 top-0 h-4 w-4" />
+                ) : sorting.order === 0 ? (
+                  <ArrowDownIcon className="absolute right-0 top-0 h-4 w-4" />
+                ) : (
+                  <ArrowUpIcon className="absolute right-0 top-0 h-4 w-4" />
+                )}
+              </button>
             </th>
           </tr>
         </thead>
         <tbody className="divide-y divide-radix-mauve6 font-semibold">
-          {playerTips.map((t) => (
-            <tr className={cn(t.info && 'violet-cta')} key={t.key}>
-              <td className="py-3 px-4 md:px-6">{t.name}</td>
+          {sortTips(playerTips, sorting).map((t) => (
+            <tr className={cn(t.info && 'violet-cta')} key={t.id}>
+              <td className="w-full py-3 px-4 md:px-6">{t.name}</td>
               <td className="text-center px-4 md:px-6">{t.tip}</td>
               <td className="text-center px-4 md:px-6">{t.points}</td>
             </tr>
