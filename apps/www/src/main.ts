0f99574dc2125f2129e5bc77a004b2ec3a8327d3
Micha Buchholz <micha@haus23.net>
2023-01-31 11:04:20 +0100

    Create initial typescript web app.

---
+++ apps/www/src/main.ts
@@ -0,0 +1,9 @@
+// Global styles
+import './styles/index.css';
+
+const appContainer = document.getElementById('app') as HTMLElement;
+
+const app = document.createElement('h1');
+app.textContent = 'runde.tips';
+
+appContainer.appendChild(app);
