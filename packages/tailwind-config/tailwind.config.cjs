0639e5f7865541496ac65c721111e04d2bd9e0c7
Micha Buchholz <micha@haus23.net>
2023-01-31 16:41:26 +0100

    Use the radix colors plugin

--- packages/tailwind-config/tailwind.config.cjs
+++ packages/tailwind-config/tailwind.config.cjs
@@ -1,6 +1,12 @@
+const radixColors = require('radix-colors-plugin');
+
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ['index.html', 'src/**/*.{ts,tsx}'],
+  darkMode: 'class',
+  plugins: [
+    radixColors({ colors: ['violet', 'mauve'], mappings: { brand: 'violet', neutral: 'mauve' } }),
+  ],
 };
 
 module.exports = config;
