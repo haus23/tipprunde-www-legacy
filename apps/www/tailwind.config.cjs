8ed543bbe6f344a509e2a95f2bc78d1a5f07b865
Micha Buchholz <micha@haus23.net>
2023-01-31 17:10:11 +0100

    Move plugin usage to www app. Can now seperate the colors usage.

--- apps/www/tailwind.config.cjs
+++ apps/www/tailwind.config.cjs
@@ -1,3 +1,9 @@
 const config = require('tailwind-config');
+const radixColors = require('radix-colors-plugin');
 
-module.exports = config;
+module.exports = {
+  ...config,
+  plugins: [
+    radixColors({ colors: ['violet', 'mauve'], mappings: { brand: 'violet', neutral: 'mauve' } }),
+  ],
+};
--- packages/tailwind-config/tailwind.config.cjs
+++ packages/tailwind-config/tailwind.config.cjs
@@ -1,12 +1,7 @@
-const radixColors = require('radix-colors-plugin');
-
 /** @type {import('tailwindcss').Config} */
 const config = {
   content: ['index.html', 'src/**/*.{ts,tsx}'],
   darkMode: 'class',
-  plugins: [
-    radixColors({ colors: ['violet', 'mauve'], mappings: { brand: 'violet', neutral: 'mauve' } }),
-  ],
 };
 
 module.exports = config;
