8e61b1c9a021a7d49fc3c507f5cc0c875723f695
Micha Buchholz <micha@haus23.net>
2023-02-01 13:46:05 +0100

    Move colors up to body

--- apps/www/src/routes/_layout.tsx
+++ apps/www/src/routes/_layout.tsx
@@ -3,7 +3,7 @@ import AppHeader from '~/components/layout/app-header';
 
 export default function RootLayout() {
   return (
-    <div className="h-full neutral-app-bg neutral-app-text-contrast">
+    <div className="h-full">
       <AppHeader />
       <main className="mx-auto max-w-5xl mt-6 sm:px-6 lg:px-8 pb-10">
         <Outlet />
