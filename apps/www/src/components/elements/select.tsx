223f887458bc359c8a1051206301f9697cbdd9a9
Micha Buchholz <micha@haus23.net>
2023-02-02 11:15:04 +0100

    Make select groupable.

--- apps/www/src/components/elements/select.tsx
+++ apps/www/src/components/elements/select.tsx
@@ -1,3 +1,4 @@
+import { Fragment, ReactElement } from 'react';
 import { CheckIcon, ChevronDownIcon, ChevronUpIcon } from '@heroicons/react/24/outline';
 import * as SelectPrimitive from '@radix-ui/react-select';
 
@@ -7,19 +8,25 @@ type ExtractStringValueKeys<T> = { [I in keyof T]: T[I] extends string ? I : nev
   string
 >];
 
-type SelectProps<T extends OptionType> = {
+type SelectProps<T extends OptionType, G extends OptionType> = {
   value: string;
   onValueChange(value: string): void;
   options: T[];
   displayValue?: ExtractStringValueKeys<T> | ((elt: T) => string);
+  groups?: G[];
+  groupKey?: ExtractStringValueKeys<T>;
+  groupDisplayValue?: ExtractStringValueKeys<G> | ((elt: G) => string);
 };
 
-export function Select<T extends OptionType>({
+export function Select<T extends OptionType, G extends OptionType>({
   value,
   onValueChange,
   options,
   displayValue,
-}: SelectProps<T>) {
+  groups,
+  groupKey,
+  groupDisplayValue,
+}: SelectProps<T, G>) {
   const displayFn = (item: T) => {
     if (typeof displayValue === 'undefined') {
       return item.id;
@@ -32,7 +39,7 @@ export function Select<T extends OptionType>({
 
   return (
     <SelectPrimitive.Root value={value} onValueChange={onValueChange}>
-      <SelectPrimitive.Trigger className="translate-y-[1px] grow sm:grow-0 basis-1/2 md:basis-1/3 inline-flex items-center justify-between font-semibold p-1 pl-2 rounded shadow brand-cta-int border-2 brand-border-int focus:outline-none focus:ring-4 focus:ring-brand7">
+      <SelectPrimitive.Trigger className="translate-y-[1px] grow sm:grow-0 basis-1/2 lg:basis-1/3 inline-flex items-center justify-between font-semibold p-1 pl-2 rounded shadow brand-cta-int border-2 brand-border-int focus:outline-none focus:ring-4 focus:ring-brand7">
         <SelectPrimitive.Value />
         <SelectPrimitive.Icon>
           <ChevronDownIcon className="h-5 w-5 brand-app-text-contrast" />
@@ -50,18 +57,45 @@ export function Select<T extends OptionType>({
             <ChevronUpIcon className="h-5 w-5" />
           </SelectPrimitive.ScrollUpButton>
           <SelectPrimitive.Viewport className="p-1">
-            {options.map((item) => (
-              <SelectPrimitive.Item
-                key={item.id}
-                value={item.id}
-                className="relative flex items-center justify-between space-x-2 my-1 py-1 pl-2 pr-8 cursor-pointer rounded focus:outline-none focus:ring-2 focus:ring-neutral7 data-[state=checked]:text-brand11"
-              >
-                <SelectPrimitive.ItemText>{displayFn(item)}</SelectPrimitive.ItemText>
-                <SelectPrimitive.ItemIndicator className="absolute right-0">
-                  <CheckIcon className="p-1 h-6 w-6" />
-                </SelectPrimitive.ItemIndicator>
-              </SelectPrimitive.Item>
-            ))}
+            {(groups || [undefined]).map((g) => {
+              let GroupContainer: ReactElement;
+              const children = options
+                .filter((item) => (groupKey ? item[groupKey] === g?.id : true))
+                .map((item) => (
+                  <SelectPrimitive.Item
+                    key={item.id}
+                    value={item.id}
+                    className="relative flex items-center justify-between space-x-2 my-1 py-1 pl-2 pr-8 cursor-pointer rounded focus:outline-none focus:ring-2 focus:ring-neutral7 data-[state=checked]:text-brand11"
+                  >
+                    <SelectPrimitive.ItemText>{displayFn(item)}</SelectPrimitive.ItemText>
+                    <SelectPrimitive.ItemIndicator className="absolute right-0">
+                      <CheckIcon className="p-1 h-6 w-6" />
+                    </SelectPrimitive.ItemIndicator>
+                  </SelectPrimitive.Item>
+                ));
+              if (g) {
+                const groupDisplayFn = (item: G) => {
+                  if (typeof groupDisplayValue === 'undefined') {
+                    return item.id;
+                  } else if (typeof groupDisplayValue === 'string') {
+                    return item[groupDisplayValue];
+                  } else if (typeof groupDisplayValue === 'function') {
+                    return groupDisplayValue(item);
+                  }
+                };
+                GroupContainer = (
+                  <SelectPrimitive.Group key={g.id}>
+                    <SelectPrimitive.Label className="text-neutral11 text-sm">
+                      {groupDisplayFn(g)}
+                    </SelectPrimitive.Label>
+                    {...children}
+                  </SelectPrimitive.Group>
+                );
+              } else {
+                GroupContainer = <Fragment key={'no-group'}>{...children}</Fragment>;
+              }
+              return GroupContainer;
+            })}
           </SelectPrimitive.Viewport>
           <SelectPrimitive.ScrollDownButton className="flex justify-center">
             <ChevronDownIcon className="h-5 w-5" />
--- apps/www/src/routes/standings/matches.tsx
+++ apps/www/src/routes/standings/matches.tsx
@@ -1,7 +1,39 @@
+import { useState } from 'react';
+import { Select } from '~/components/elements/select';
+import { useChampionship } from '~/hooks/use-championship';
+import { useMasterdata } from '~/hooks/use-masterdata';
+import { useStandings } from '~/hooks/use-standings';
+
 export default function Matches() {
+  const { teams } = useMasterdata();
+  const championship = useChampionship();
+  const { matches, rounds } = useStandings();
+
+  const [matchId, setMatchId] = useState(matches[0].id);
+  const match = matches.find((m) => m.id === matchId);
+
   return (
     <>
-      <h2>Spiele</h2>
+      <header className="flex items-center gap-x-4 mx-2 sm:mx-0">
+        <h2 className="flex gap-x-2 text-xl font-semibold tracking-tight">
+          <span className="hidden sm:block">{championship.name} -</span>
+          <span>Tipps für</span>
+          <span className="sr-only">{`${match && teams[match.hometeamId].name} - ${
+            match && teams[match.awayteamId].name
+          }`}</span>
+        </h2>
+        <Select
+          options={matches}
+          value={matchId}
+          onValueChange={setMatchId}
+          displayValue={(item) =>
+            `${teams[item.hometeamId].shortname} - ${teams[item.awayteamId].shortname}`
+          }
+          groups={rounds}
+          groupKey="roundId"
+          groupDisplayValue={(item) => `Runde ${item.nr}`}
+        />
+      </header>
     </>
   );
 }
