7925fd4a18ca16ef4b40ffcf0d9ea1c79b6bdc0a
Micha Buchholz <micha@haus23.net>
2023-02-15 16:36:07 +0100

    Add routing animations

--- apps/www/src/routes/_layout.tsx
+++ apps/www/src/routes/_layout.tsx
@@ -1,5 +1,6 @@
 import { MasterData } from '@haus23/dtp-types';
-import { Outlet, ScrollRestoration } from 'react-router-dom';
+import { AnimatePresence, motion } from 'framer-motion';
+import { Outlet, ScrollRestoration, useLocation } from 'react-router-dom';
 import AppHeader from '~/components/layout/app-header';
 
 export type LoaderReturnType = MasterData;
@@ -11,12 +12,22 @@ export async function loader() {
 export const shouldRevalidateFunction = () => false;
 
 export default function RootLayout() {
+  const location = useLocation();
   return (
     <div className="h-full">
       <AppHeader />
-      <main className="mx-auto max-w-5xl mt-6 sm:px-6 lg:px-8 pb-10">
-        <Outlet />
-      </main>
+      <AnimatePresence mode="wait" initial={false}>
+        <motion.main
+          key={location.pathname}
+          initial={{ x: '+10%', opacity: 0 }}
+          animate={{ x: '0', opacity: 1 }}
+          exit={{ x: '+10%', opacity: 0 }}
+          transition={{ duration: 0.1 }}
+          className="mx-auto max-w-5xl mt-6 sm:px-6 lg:px-8 pb-10"
+        >
+          <Outlet />
+        </motion.main>
+      </AnimatePresence>
       <ScrollRestoration />
     </div>
   );
