e3d3a1485b18c24337692c4f2a1c7f64adaf81b1
Micha Buchholz <mail@lean-stack.de>
2023-03-04 13:46:21 +0100

    Initialize firebase.

--- apps/www/src/main.tsx
+++ apps/www/src/main.tsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './app';
+import { initializeApp } from './firebase/initialize-app';
 
 // Global styles
 import './styles/index.css';
+
+// Firebase
+initializeApp();
 
 const appContainer = document.getElementById('app') as HTMLElement;
 
